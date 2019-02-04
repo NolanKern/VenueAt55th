235e5e3c6534eba440a8f42802737e947a1a10d5
NolanKern <nkern99@gmail.com>
2019-02-04 14:12:06 -0800

    weekend work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 // Questions Array
 const questions = [
-  { question: 'Enter Your Name' },
-  { question: 'Enter Your Preferred Contact Email', pattern: /\S+@\S+\.\S+/ },
-  { question: 'What Type Of Event Would You Be Hosting' },
+  { question: "Enter Your Name" },
+  { question: "Enter Your Preferred Contact Email", pattern: /\S+@\S+\.\S+/ },
+  { question: "What Type Of Event Would You Be Hosting" },
   {
     question: "Please Select A Date For Your Event",
     pattern:
@@ -17,7 +17,7 @@ const questions = [
       /^((0|1)\d{1})(\/)((0|1|2|3)\d{1})(\/)((19|20)\d{2})- ((0|1)\d{1})(\/)((0|1|2|3)\d{1})(\/)((19|20)\d{2})/ |
       /^((0|1)\d{1})(\/)((0|1|2|3)\d{1})(\/)((19|20)\d{2}) - ((0|1)\d{1})(\/)((0|1|2|3)\d{1})(\/)((19|20)\d{2})/
   },
-  { question: 'Send A Message' }
+  { question: "Send A Message" }
 ];
 
 // Transition Times
@@ -28,33 +28,35 @@ const switchTime = 200; // Transition Between Questions
 let position = 0;
 
 // Init DOM Elements
-const formBox = document.querySelector('#form-box');
-const nextBtn = document.querySelector('#next-btn');
-const prevBtn = document.querySelector('#prev-btn');
-const inputGroup = document.querySelector('#input-group');
-const inputField = document.querySelector('#input-field');
-const inputLabel = document.querySelector('#input-label');
-const inputProgress = document.querySelector('#input-progress');
-const progress = document.querySelector('#progress-bar');
-const calBtn = document.querySelector('#cal-btn');
+const formBox = document.querySelector("#form-box");
+const nextBtn = document.querySelector("#next-btn");
+const prevBtn = document.querySelector("#prev-btn");
+const inputGroup = document.querySelector("#input-group");
+const inputField = document.querySelector("#input-field");
+const inputLabel = document.querySelector("#input-label");
+const inputProgress = document.querySelector("#input-progress");
+const progress = document.querySelector("#progress-bar");
+const calBtn = document.querySelector("#cal-btn");
+const contactUS = document.querySelector("#contact");
 
 // EVENTS
 
 // Get Question On DOM Load
-document.addEventListener('DOMContentLoaded', getQuestion);
+document.addEventListener("DOMContentLoaded", getQuestion);
 
 // Next Button Click
-nextBtn.addEventListener('click', validate);
-
+nextBtn.addEventListener("click", validate);
 
 // Previous Button Click
-prevBtn.addEventListener('click', back);
+prevBtn.addEventListener("click", back);
+
+contactUS.addEventListener('click',toggle);
 
 // Calendar button
 // calBtn.addEventListener('click',log);
 
 // Input Field Enter Click
-inputField.addEventListener('keyup', e => {
+inputField.addEventListener("keyup", e => {
   if (e.keyCode == 13) {
     validate();
   }
@@ -63,8 +65,8 @@ inputField.addEventListener('keyup', e => {
 // FUNCTIONS
 
 // Test for the calendar button working
-function log(){
-  console.log('it worked');
+function log() {
+  console.log("it worked");
 }
 
 // Get Question From Array & Add To Markup
@@ -72,48 +74,45 @@ function getQuestion() {
   // Get Current Question
   inputLabel.innerHTML = questions[position].question;
   // Get Current Type
-  inputField.type = questions[position].type || 'text';
+  inputField.type = questions[position].type || "text";
   // Get Current Answer
-  inputField.value = questions[position].answer || '';
+  inputField.value = questions[position].answer || "";
   // Focus On Element
   // inputField.focus();
 
   // Set Progress Bar Width - Variable to the questions length
-  progress.style.width = (position * 100) / questions.length + '%';
+  progress.style.width = (position * 100) / questions.length + "%";
 
   // Add User Icon OR Back Arrow Depending On Question
-  prevBtn.className = position ? 'fas fa-arrow-left' : 'fas fa-user';
+  prevBtn.className = position ? "fas fa-arrow-left" : "fas fa-user";
 
   // Check to see if its the date question
-  if(position === 3){
+  if (position === 3) {
     calBtn.style.opacity = 1;
-    calBtn.addEventListener('click',log);
+    calBtn.addEventListener("click", log);
   }
 
   showQuestion();
 }
 
-
 // WORK NEEDED HERE
-function back(){
+function back() {
   // if position === 3 hide calendar
-  if(position === 3){
-    calBtn.style.opacity=0;
-    calBtn.removeEventListener('click');
+  if (position === 3) {
+    calBtn.style.opacity = 0;
+    calBtn.removeEventListener("click");
   }
   // Move back on question array
   position--;
   getQuestion();
 }
 
-
-
 // WORK NEEDED HERE CHECK FOR POSITION THEN SET OPACITY OF CALENDAR
 // Display Question To User
 function showQuestion() {
   inputGroup.style.opacity = 1;
-  inputProgress.style.transition = '';
-  inputProgress.style.width = '100%';
+  inputProgress.style.transition = "";
+  inputProgress.style.width = "100%";
 }
 
 // Hide Question From User
@@ -121,7 +120,7 @@ function hideQuestion() {
   inputGroup.style.opacity = 0;
   inputLabel.style.marginLeft = 0;
   inputProgress.style.width = 0;
-  inputProgress.style.transition = 'none';
+  inputProgress.style.transition = "none";
   inputGroup.style.border = null;
 }
 
@@ -142,7 +141,7 @@ function validate() {
 
 // Field Input Fail
 function inputFail() {
-  formBox.className = 'error';
+  formBox.className = "error";
   // Repeat Shake Motion -  Set i to number of shakes
   for (let i = 0; i < 6; i++) {
     setTimeout(transform, shakeTime * i, ((i % 2) * 2 - 1) * 20, 0);
@@ -153,7 +152,7 @@ function inputFail() {
 
 // Field Input Passed
 function inputPass() {
-  formBox.className = '';
+  formBox.className = "";
   setTimeout(transform, shakeTime * 0, 0, 10);
   setTimeout(transform, shakeTime * 1, 0, 0);
 
@@ -170,8 +169,8 @@ function inputPass() {
   } else {
     // Remove If No More Questions
     hideQuestion();
-    formBox.className = 'close';
-    progress.style.width = '100%';
+    formBox.className = "close";
+    progress.style.width = "100%";
 
     // Form Complete
     formComplete();
@@ -180,8 +179,8 @@ function inputPass() {
 
 // All Fields Complete - Show h1 end
 function formComplete() {
-  const h1 = document.createElement('h1');
-  h1.classList.add('end');
+  const h1 = document.createElement("h1");
+  h1.classList.add("end");
   h1.appendChild(
     document.createTextNode(
       `Thanks ${
@@ -194,35 +193,219 @@ function formComplete() {
     setTimeout(() => (h1.style.opacity = 1), 50);
   }, 1000);
 
-  window.location.href = 'mailto:thevenue@ens.com?subject=Venue Booking Request '+ questions[3].answer + '&body=From '+ questions[0].answer + ', %0D%0A I would like to enquire about hosting a ' + questions[2].answer + ' event on '+ questions[3].answer + '. ' + questions[4].answer + '. %0D%0A With Regards, %0D%0A '+ questions[0].answer;
+  window.location.href =
+    "mailto:thevenue@ens.com?subject=Venue Booking Request " +
+    questions[3].answer +
+    "&body=From " +
+    questions[0].answer +
+    ", %0D%0A I would like to enquire about hosting a " +
+    questions[2].answer +
+    " event on " +
+    questions[3].answer +
+    ". " +
+    questions[4].answer +
+    ". %0D%0A With Regards, %0D%0A " +
+    questions[0].answer;
 }
 
 // NEED TO CREATE FUNCTION TO SEND EMAIL USING ANSWERS FROM QUESTIONS[I].ANSWERS AND ITTERATE THROUGH
-
-
-
-
 
 /*Scroll to top when arrow up clicked BEGIN*/
 $(window).scroll(function() {
   var height = $(window).scrollTop();
   if (height > 100) {
-      $('#back2Top').fadeIn();
+    $("#back2Top").fadeIn();
   } else {
-      $('#back2Top').fadeOut();
+    $("#back2Top").fadeOut();
   }
 });
 $(document).ready(function() {
   $("#back2Top").click(function(event) {
-      event.preventDefault();
-      $("html, body").animate({ scrollTop: 0 }, "slow");
-      return false;
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
   });
-
 });
 
 $(".btn--contact").click(function() {
-  $([document.documentElement, document.body]).animate({
+  $([document.documentElement, document.body]).animate(
+    {
       scrollTop: $(".section-book").offset().top
-  }, 2000);
+    },
+    2000
+  );
 });
+
+
+function toggle() {
+  event.preventDefault();
+  console.log('made it');
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    console.log('here');
+    window.open('tel:4805051111');
+    
+  } else {
+    contactUS.click(e => {
+      console.log('therte');
+      contactUS.click(e => {
+        document.write(
+          "<" +
+            "a" +
+            " " +
+            "h" +
+            "r" +
+            "e" +
+            "f" +
+            "=" +
+            "'" +
+            "m" +
+            "&" +
+            "#" +
+            "9" +
+            "7" +
+            ";" +
+            "i" +
+            "l" +
+            "t" +
+            "o" +
+            "&" +
+            "#" +
+            "5" +
+            "8" +
+            ";" +
+            "t" +
+            "&" +
+            "#" +
+            "1" +
+            "0" +
+            "4" +
+            ";" +
+            "%" +
+            "&" +
+            "#" +
+            "5" +
+            "4" +
+            ";" +
+            "5" +
+            "&" +
+            "#" +
+            "1" +
+            "1" +
+            "8" +
+            ";" +
+            "e" +
+            "%" +
+            "6" +
+            "E" +
+            "&" +
+            "#" +
+            "1" +
+            "1" +
+            "7" +
+            ";" +
+            "e" +
+            "&" +
+            "#" +
+            "6" +
+            "4" +
+            ";" +
+            "e" +
+            "n" +
+            "&" +
+            "#" +
+            "3" +
+            "7" +
+            ";" +
+            "&" +
+            "#" +
+            "5" +
+            "5" +
+            ";" +
+            "3" +
+            "&" +
+            "#" +
+            "4" +
+            "6" +
+            ";" +
+            "c" +
+            "&" +
+            "#" +
+            "1" +
+            "1" +
+            "1" +
+            ";" +
+            "m" +
+            "'" +
+            ">" +
+            "t" +
+            "&" +
+            "#" +
+            "1" +
+            "0" +
+            "4" +
+            ";" +
+            "&" +
+            "#" +
+            "1" +
+            "0" +
+            "1" +
+            ";" +
+            "&" +
+            "#" +
+            "1" +
+            "1" +
+            "8" +
+            ";" +
+            "e" +
+            "&" +
+            "#" +
+            "1" +
+            "1" +
+            "0" +
+            ";" +
+            "u" +
+            "e" +
+            "&" +
+            "#" +
+            "6" +
+            "4" +
+            ";" +
+            "e" +
+            "&" +
+            "#" +
+            "1" +
+            "1" +
+            "0" +
+            ";" +
+            "s" +
+            "&" +
+            "#" +
+            "4" +
+            "6" +
+            ";" +
+            "c" +
+            "&" +
+            "#" +
+            "1" +
+            "1" +
+            "1" +
+            ";" +
+            "m" +
+            "<" +
+            "/" +
+            "a" +
+            ">"
+        );
+      });
+    });
+    
+  }
+}
+
+function redirect() {
+  window.location.replace('../../gallery.html');
+}
